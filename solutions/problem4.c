25700ba6de78ddeace7ab4cae46d571f9a59d1de
fprintf <fprintfhome@gmail.com>
2015-02-02 21:27:22 -0800

    problem4 [C] uses string traversal to check for palindrome

---
+++ solutions/problem4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h> 
+
+bool ispalindrome(size_t number)
+{
+    char s[BUFSIZ];
+    size_t bytes = snprintf(s,BUFSIZ,"%zu",number);
+
+    if (bytes <= 0 || bytes >= BUFSIZ) {
+        fprintf(stderr, "Failed to check %zu exceeds length of string\n", number);
+        return false;
+    }
+
+    /* Check if the string is the same forwards and backwards */
+    char * start = s;
+    char * end = s + strlen(s) - 1;
+
+    while (start < end) {
+        if (*start++ != *end--)
+            return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char ** argv)
+{
+    size_t num1, num2;
+    size_t best = 0;
+
+    for (num1 = 999; num1 >= 100; --num1)
+        for (num2 = 999; num2 >= 100; --num2) {
+            size_t product = num1 * num2;
+            if (ispalindrome(product) && product > best) {
+                best = product;
+            }
+        }
+
+    printf("Found palindromic product: %zu\n", best);
+            
+
+	return 0;
+}
